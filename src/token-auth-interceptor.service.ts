42ce7753cd3cf91d0ca750ad8022a91cbf482e84
kruczjak <kruczjak@gmail.com>
2017-08-06 13:39:09 +0200

    Handle reponse headers even on HTTP errors

--- src/token-auth-interceptor.service.ts
+++ src/token-auth-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpHeaderResponse, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { AuthData, TokenStorageService } from './token-storage.service';
 import 'rxjs/add/operator/do'
@@ -15,19 +15,22 @@ export class TokenAuthInterceptorService implements HttpInterceptor {
 
     const authReq = req.clone({ setHeaders: authHeaders });
 
-    return next.handle(authReq).do((event) => this.handleResponseEvent(event));
+    return next.handle(authReq)
+      .do((event) => this.handleResponseEvent(event), (event) => this.handleResponseEvent(event));
   }
 
   private handleResponseEvent(event: HttpEvent<any>) {
-    if (!(event instanceof HttpHeaderResponse)) { return; }
+    if (!event.hasOwnProperty('headers')) { return; }
 
-    const responseHeaders = event.headers;
+    const responseHeaders = <HttpHeaders> event['headers'];
+    if (!responseHeaders.has('access-token')) { return; }
 
     const authData: AuthData = {};
     TokenStorageService.AUTH_KEYS.forEach((key) => {
       authData[key] = responseHeaders.get(TokenStorageService.AUTH_HEADERS_MAPPING[key] || key);
     });
 
+    if (!this.authTokenStorage.isAuthDataValid(authData)) { return; }
     this.authTokenStorage.loadAuthData(authData);
   }
 }

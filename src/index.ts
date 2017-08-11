1a4fa2f952f6e3f7b20b44e7fd51b621b4ee1ee1
kruczjak <kruczjak@gmail.com>
2017-08-11 23:43:42 +0200

    Update ngx-token-auth to 0.1.7

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { NgModule, ModuleWithProviders, Provider } from '@angular/core';
+import { NgModule, ModuleWithProviders, Provider, InjectionToken } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TokenAuthService } from './token-auth.service';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpInterceptor } from '@angular/common/http';
 import { TokenAuthInterceptorService } from './token-auth-interceptor.service';
 import { TokenStorageService } from './token-storage.service';
 import { TokenAuthConfigService } from './token-auth-config.service';
@@ -9,6 +9,14 @@ import { TokenAuthConfigService } from './token-auth-config.service';
 export * from './token-auth.service';
 export * from './token-auth-config.abstract';
 export * from './token-auth-config.service';
+
+export const TOKEN_SERVICE_PROVIDER = { provide: TokenAuthConfigService, useClass: TokenAuthConfigService };
+
+export const CUSTOM_HTTP_INTERCEPTOR = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: TokenAuthInterceptorService,
+  multi: true
+};
 
 @NgModule({
   imports: [
@@ -23,8 +31,8 @@ export class TokenAuthModule {
       providers: [
         TokenAuthService,
         TokenStorageService,
-        tokenAuthConfig || { provide: TokenAuthConfigService, useClass: TokenAuthConfigService },
-        { provide: HTTP_INTERCEPTORS, useClass: TokenAuthInterceptorService, multi: true }
+        tokenAuthConfig || TOKEN_SERVICE_PROVIDER,
+        CUSTOM_HTTP_INTERCEPTOR
       ]
     };
   }
